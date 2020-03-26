0c0d64f6271a83a99dad69d1afa8189c836820a6
Colin Cousins <cacousins@gmail.com>
2020-03-26 19:24:21 -0400

    Do While Menu Example

---
+++ src/dowhileloopclass/DoWhileMenu.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dowhileloopclass;
+
+/**
+ *
+ * @author cacousins
+ */
+public class DoWhileMenu {
+
+    public static void main(String[] args) {
+        //create a menu
+        //with four possible options
+        //if the user does not choose a correct option then the
+        //the loop continues
+        //sentinel value - 0
+        //once a correct value is chosen, then it jumps to a switch
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        int choice;
+
+        do {
+            //create the menu for the app
+            System.out.println("Calulator Program");
+            System.out.println("1. Addition");
+            System.out.println("2. Multiplication");
+            System.out.println("3. Division");
+            System.out.println("4. Substraction");
+            System.out.println("choose an opion, 0 to exit");
+            choice = input.nextInt();
+
+        } while (choice < 0 || choice > 4);
+
+        switch (choice) {
+            case 1:
+                System.out.println("Addition");
+                break;
+            case 2:
+                System.out.println("Multi");
+                break;
+            case 3:
+                System.out.println("Divi");
+                break;
+            case 4:
+                System.out.println("Sub");
+                break;
+            case 0:
+                System.out.println("Goodbye ...");
+                break;
+
+        }
+    }
+
+}
